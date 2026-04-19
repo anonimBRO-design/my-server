061b4ea26e29c8a5a2ffad72a6af7712958bc36d
anonimBRO-design <axiooxjkt48pro@gmail.com>
2026-04-19 11:54:58 +0700

    add all files

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+// route utama
+app.get("/", (req, res) => {
+  res.send("Halo dari server Node.js 🚀");
+});
+
+// port (WAJIB buat deploy)
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server jalan di port " + PORT);
+});
