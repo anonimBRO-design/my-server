69e75ffcdad24e2b1b66694604cb7ea391d41bcf
anonimBRO-design <axiooxjkt48pro@gmail.com>
2026-04-19 12:27:25 +0700

    serve static html

--- server.js
+++ server.js
@@ -1,22 +1,8 @@
 const express = require("express");
 const app = express();
-
-// route utama
-app.get("/", (req, res) => {
-  res.send("Halo dari server Node.js 🚀");
-});
-
-// port (WAJIB buat deploy)
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log("Server jalan di port " + PORT);
-});const express = require("express");
-const app = express();
 const path = require("path");
 
-// Serve folder public
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(__dirname));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
