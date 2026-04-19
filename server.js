781c2568724b9b851601f8cd53830f7ed16e841e
anonimBRO-design <axiooxjkt48pro@gmail.com>
2026-04-19 12:35:38 +0700

    fix static folder

--- server.js
+++ server.js
@@ -7,4 +7,13 @@ app.use(express.static(__dirname));
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Server jalan di port " + PORT);
+});const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server jalan di port " + PORT);
 });
